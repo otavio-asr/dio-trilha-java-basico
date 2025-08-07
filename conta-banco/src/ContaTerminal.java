6f9992b0387f7af5109af8d73056c7abc0b8d5a4
Otavio Augusto <otavioasr2006@gmail.com>
2025-08-07 17:23:34 -0300

    Criando TODOS

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,13 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        //TODO: "Importar a classe scanner"
+
+        //Exibir as mensagens para o nosso usuario
+
+        //Obter pela scanner os valores digitados no terminal
+
+        //Exibir a mesnsagem conta criada
+        
     }
 }
